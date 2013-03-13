301e014fd207f5cd9e95562178247219294cda25
zackn9ne <zackn9ne@gmail.com>
2013-03-13 13:08:40 -0300

    Update content-bx.php
    if content 2, if content 3, etc..

--- content-bx.php
+++ content-bx.php
@@ -1,42 +1,58 @@
-<?php
-/**
- * The template used for displaying page content in page.php
- *
- * @package sedona
- * @since sedona 1.0
- */
-?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div><!-- .entry-content -->
-
-	
-
-	<?php $songs = get_post_meta($post->ID, 'slide', false); ?>
-	
-	<ul class="bxslider">
-	<?php foreach($songs as $song) {
-	echo '<li>'.$song.'</li>';
-	} ?>
-	</ul>
-<div class="outside">
-  <h3>This div is outside of the slider</h3>
-  <p><span id="slider-prev"></span> | <span id="slider-next"></span></p>
-</div>
-
-
-	<section id="section-nav">
-
-
-			<!-- <a href="<?php $permalink = get_permalink($post->post_parent); ?>just-the-facts/" id="just-the-facts" class="button">Just the facts</a> -->
-			<a href="../just-the-facts/" id="just-the-facts" class="button">Just the facts</a>
-			
-
-	</section> <!-- /#section-nav -->
-</article><!-- #post-<?php the_ID(); ?> -->
+ <!-- Carousel items -->
+                  <div class="carousel-inner">
+                        <div class="active item">
+                                <img src="<?php echo get_post_meta($post->ID, "image", true); ?>"
+                                <?php echo get_post_meta($post->ID, 'content', true); ?>
+                        </div>
+ 
+                        <?php if( get_post_meta($post->ID, 'content3', true) )  { ?>
+                        <div class="item">
+                                <img src="<?php echo get_post_meta($post->ID, "image2", true); ?>" />
+                                <?php echo get_post_meta($post->ID, 'content2', true); ?>
+                        </div>
+                        <?php } ?>
+                       
+                        <?php if( get_post_meta($post->ID, 'content3', true) ) { ?>
+                        <div class="item">
+                                <img src="<?php echo get_post_meta($post->ID, "image3", true); ?>" />
+                                <?php echo get_post_meta($post->ID, 'content3', true); ?>
+                        </div>
+                        <?php } ?>
+                   
+                        <?php if( get_post_meta($post->ID, 'content4', true) ) { ?>
+                        <div class="item">
+                                <img src="<?php echo get_post_meta($post->ID, "image4", true); ?>" />
+                                <?php echo get_post_meta($post->ID, 'content4', true); ?>
+                        </div>
+                        <?php } ?>
+ 
+                        <?php if( get_post_meta($post->ID, 'content5', true) ) { ?>
+                        <div class="item">
+                                <img src="<?php echo get_post_meta($post->ID, "image5", true); ?>" />
+                                <?php echo get_post_meta($post->ID, 'content5', true); ?>
+                        </div>
+                        <?php } ?>
+ 
+                        <?php if( get_post_meta($post->ID, 'content6', true) ) { ?>
+                        <div class="item">
+                                <img src="<?php echo get_post_meta($post->ID, "image6", true); ?>" />
+                                <?php echo get_post_meta($post->ID, 'content6', true); ?>
+                        </div>
+                        <?php } ?>
+ 
+                        <?php if( get_post_meta($post->ID, 'content7', true) ) { ?>
+                        <div class="item">
+                                <img src="<?php echo get_post_meta($post->ID, "image7", true); ?>" />
+                                <?php echo get_post_meta($post->ID, 'content7', true); ?>
+                        </div>
+                        <?php } ?>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+                    <div class="item">…</div>
+                  </div>
