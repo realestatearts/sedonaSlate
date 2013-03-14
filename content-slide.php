2f48e87cd12f4e220839608f7e5b9d25f076a666
zackn9ne <zackn9ne@gmail.com>
2013-03-14 12:54:45 -0300

    Update content-slide.php

--- content-slide.php
+++ content-slide.php
@@ -1,4 +1,4 @@
-	<h1 class="pageTitle"><?php the_title(); ?></h1>
+
 		<?php the_content(); ?>
 
 
@@ -10,44 +10,62 @@
 		    <li data-target="#myCarousel" data-slide-to="2"></li>
 		  </ol>
 
+<!-- Carousel items -->
+      <div class="carousel-inner contentSlide">
+            <div class="active item">
+                    <div class="nivo-caption"><?php echo get_post_meta($post->ID, 'caption', true); ?></div>
+                    <img src="<?php echo get_post_meta($post->ID, "image", true); ?>">
+                    <div class="entry-content"><?php echo get_post_meta($post->ID, 'content', true); ?></a></div>
+            </div>
 
-asdf
+            <?php if( get_post_meta($post->ID, 'content2', true) )  { ?>
+            <div class="item">
+                   <div class="nivo-caption"><?php echo get_post_meta($post->ID, 'caption2', true); ?></div>
+                   <img src="<?php echo get_post_meta($post->ID, "image2", true); ?>" />
+                    <div class="entry-content"><?php echo get_post_meta($post->ID, 'content2', true); ?></a></div>
+            </div>
+            <?php } ?>
+           
+            <?php if( get_post_meta($post->ID, 'content3', true) ) { ?>
+            <div class="item">
+                  <div class="nivo-caption"><?php echo get_post_meta($post->ID, 'caption3', true); ?></div>
+                  <img src="<?php echo get_post_meta($post->ID, "image3", true); ?>" />
+                    <div class="entry-content"><?php echo get_post_meta($post->ID, 'content3', true); ?></a></div>
+            </div>
+            <?php } ?>
+       
+            <?php if( get_post_meta($post->ID, 'content4', true) ) { ?>
+            <div class="item">
+                  <div class="nivo-caption"><?php echo get_post_meta($post->ID, 'caption4', true); ?></div>
+                    <img src="<?php echo get_post_meta($post->ID, "image4", true); ?>" />
+                    <div class="entry-content"><?php echo get_post_meta($post->ID, 'content4', true); ?></a></div>
+            </div>
+            <?php } ?>
 
-<div class="carousel-inner">
-	<div class="active item">
+            <?php if( get_post_meta($post->ID, 'content5', true) ) { ?>
+            <div class="item">
+                  <div class="nivo-caption"><?php echo get_post_meta($post->ID, 'caption5', true); ?></div>
+                    <img src="<?php echo get_post_meta($post->ID, "image5", true); ?>" />
+                    <div class="entry-content"><?php echo get_post_meta($post->ID, 'content5', true); ?></a></div>
+            </div>
+            <?php } ?>
 
+            <?php if( get_post_meta($post->ID, 'content6', true) ) { ?>
+            <div class="item">
+                  <div class="nivo-caption"><?php echo get_post_meta($post->ID, 'caption6', true); ?></div>
+                    <img src="<?php echo get_post_meta($post->ID, "image6", true); ?>" />
+                    <div class="entry-content"><?php echo get_post_meta($post->ID, 'content6', true); ?></a></div>
+            </div>
+            <?php } ?>
 
-			<?php $image1 = get_post_meta($post->ID, 'image1', false); ?>
-
-			<?php foreach($image1 as $image1) {
-				echo '<img src="'.$image1.'">asdf';
-				} ?>
-
-			<?php $content1 = get_post_meta($post->ID, 'content1', false); ?>
-
-			<?php foreach($content1 as $content1) {
-				echo ''.$content1.'</div>';
-				} ?>
-
-
-
- <?php if( get_post_meta($post->ID, 'content', true) )  { ?>
- <div class="item">
-			<?php $image = get_post_meta($post->ID, 'image', false); ?>
-
-			<?php foreach($image as $image) {
-				echo '	<img src="'.$image.'">';
-				} ?>
-
-			<?php $content = get_post_meta($post->ID, 'content', false); ?>
-
-			<?php foreach($content as $content) {
-				echo ''.$content.$image.'</div> <div class="item">';
-				} ?>
-
-<?php } ?>
-</div>
-
+            <?php if( get_post_meta($post->ID, 'content7', true) ) { ?>
+            <div class="item">
+                  <div class="nivo-caption"><?php echo get_post_meta($post->ID, 'caption7', true); ?></div>
+                    <img src="<?php echo get_post_meta($post->ID, "image7", true); ?>" />
+                    <div class="entry-content"><?php echo get_post_meta($post->ID, 'content7', true); ?></a></div>
+            </div>
+            <?php } ?>
+		</div>
 
 
 
@@ -55,6 +73,7 @@ asdf
 		  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
 		  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
 		</div>
+
 		
 		
 								
@@ -63,8 +82,9 @@ asdf
 	
 	
 	<!-- Button to trigger modal -->
-	<a href="#myModal" role="button" class="btn" data-toggle="modal">Launch demo modal</a>
-
+    <section id="section-nav">
+            <a href="#myModal" id="just-the-facts" class="button" data-toggle="modal">Just the facts</a>
+    </section> 
 	<!-- Modal -->
 	<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	  <div class="modal-header">
@@ -76,14 +96,7 @@ asdf
 	  </div>
 	  <div class="modal-footer">
 	    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-	    <button class="btn btn-primary">Save changes</button>
 	  </div>
 	</div>
 <?php } ?>
 	
-
-
-
-	<section id="section-nav">
-			<a href="../just-the-facts/" id="just-the-facts" class="button">Just the facts</a>
-	</section> <!-- /#section-nav -->
